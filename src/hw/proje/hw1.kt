0c345b0e7573b32240429759257ce818e54f72f3
Turdugul <okonbaevaturdugul@gmail.com>
2021-12-07 00:39:10 +0600

    hw1

--- src/hw/proje/hw1.kt
+++ src/hw/proje/hw1.kt
@@ -1,4 +1,55 @@
 package hw.proje
+import java.lang.Exception
+
 fun main() {
-    //dfa
+    println("print the number: ")
+    val a = readLine().parseToInt()
+
+    println("output the operation: ")
+    val symbols = readLine()?.symbols()
+
+
+    println("print the number: ")
+    val b = readLine().parseToInt()
+
+
+    val sum = when (symbols) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> a / b
+        else -> "try again"
+    }
+    println("$sum")
+}
+private fun String?.parseToInt(): Int {
+    if (this == null) return  0
+    if (this == "") return  0
+
+    return  try {
+        this.toInt()
+    } catch (e:NumberFormatException){
+        println("Exception = $e")
+        0
+    }
+}
+private fun String.symbols(): String{
+    when (this) {
+        "+" -> '+'
+        "-" -> '-'
+        "*" -> '*'
+        "/" -> '/'
+        else -> "try again"
+    }
+    return this
+}
+private fun divide(a: Int, b: Int){
+    println(checkingForZero(a,b))
+}
+private fun checkingForZero(firstOne: Int, secondOne: Int): String{
+    return try {
+        "Sum: numOne/numTwo"
+    } catch (e: Exception){
+        "you can not divide by zero"
+    }
 }
