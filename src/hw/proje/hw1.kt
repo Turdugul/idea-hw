bb04ae895dd5c854e85798868694b9944573a426
Turdugul <okonbaevaturdugul@gmail.com>
2021-12-07 22:25:36 +0600

    2homework

--- src/hw/proje/hw1.kt
+++ src/hw/proje/hw1.kt
@@ -1,4 +1,19 @@
 package hw.proje
+
+//Okonbaeva Turdugul 2/homework
+
+
 fun main() {
-    //dfa
+    val num = readLine()!!.toInt()
+    val x0 = num / 1000 % 10
+    val x1 = num / 100 % 10
+    val x2 = num / 10 % 10
+    val x3 = num % 10
+
+    val result = x0+x1 == x2+x3
+
+    println(result)
+
+
 }
+
