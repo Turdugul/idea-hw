7b990ea9f7264d8e59d2cf603c21d17265efe870
Turdugul <okonbaevaturdugul@gmail.com>
2021-12-07 20:16:07 +0600

    homework 4

--- src/hw/proje/hw1.kt
+++ src/hw/proje/hw1.kt
@@ -1,4 +1,26 @@
 package hw.proje
+
+//Оконбаева Турдугүл.homework4
+
 fun main() {
-    //dfa
+    var count = 0
+    var number = readLine().parseToInt()
+    while (number != 0){
+        number /= 10
+        ++count
+    }
+    println("колчество цифр -> $count")
+}
+private fun String?.parseToInt(): Int {
+    if (this == null) return 0
+    if (this == "") return 0
+
+    return try {
+        this.toInt()
+    }catch (e: NumberFormatException) {
+        println("exeption=$e")
+        0
+    }catch (e: NumberFormatException){
+        0
+    }
 }
